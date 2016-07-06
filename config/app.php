e958f65a6097b187a6def8c321d8a5b385e1558a
jupitech <jupitech.dev@gmail.com>
2016-07-06 15:25:04 -0600

    Agregando plugin fecha y upload imagenes

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
          */
         Bican\Roles\RolesServiceProvider::class,
 
+        Intervention\Image\ImageServiceProvider::class,
+
     ],
 
     /*
@@ -209,6 +211,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
