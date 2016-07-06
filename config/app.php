62c296cb3c03e70e6aa1e5a61fff0eb3ec4ec57c
jupitech <jupitech.dev@gmail.com>
2016-07-06 15:40:29 -0600

    Area de upload imagenes

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
          * Third Party Service Providers...
          */
         Bican\Roles\RolesServiceProvider::class,
-
+        Intervention\Image\ImageServiceProvider::class,
     ],
 
     /*
@@ -209,6 +209,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
