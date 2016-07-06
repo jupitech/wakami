c7433542efb355c353e2da83c33f2ddd74de890f
jupitech <jupitech.dev@gmail.com>
2016-07-06 15:34:42 -0600

    Revert "Agregando plugin fecha y upload imagenes"
    This reverts commit e958f65a6097b187a6def8c321d8a5b385e1558a.

--- config/app.php
+++ config/app.php
@@ -162,8 +162,6 @@ return [
          */
         Bican\Roles\RolesServiceProvider::class,
 
-        Intervention\Image\ImageServiceProvider::class,
-
     ],
 
     /*
@@ -211,7 +209,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
--- gulpfile.js
+++ gulpfile.js
@@ -22,15 +22,11 @@ elixir(function(mix) {
     mix.copy(npmDir + 'angular/angular.min.js',jsDir);
     mix.copy(npmDir + 'angular-route/angular-route.min.js',jsDir);
     mix.copy(npmDir + 'angular-cookies/angular-cookies.min.js',jsDir);
-    mix.copy(npmDir + 'angular-animate/angular-animate.min.js',jsDir);
-    mix.copy(npmDir + 'angular-touch/angular-touch.min.js',jsDir);
     mix.scripts([
     	'vue.min.js',
     	'vue-resource.min.js',
     	'angular.min.js',
     	'angular-route.min.js',
-        'angular-cookies.min.js',
-        'angular-animate.min.js',
-        'angular-touch.min.js'
+        'angular-cookies.min.js'
     ], 'public/js/vendor.js');	
 });
