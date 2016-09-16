56cf9f7cf718892cb87c6f9d419de96d8ed80b5e
jupitech <jupitech.dev@gmail.com>
2016-09-16 12:06:15 -0600

    Cambios en carga de imagenes

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ var wApp= angular.module('wApp', ['ngRoute', 'ngCookies','ngAnimate','ngResource
 wApp.config(['lazyImgConfigProvider', function(lazyImgConfigProvider){
     var scrollable = document.querySelector('#scrollable');
     lazyImgConfigProvider.setOptions({
-      offset: 100, // how early you want to load image (default = 100)
+      offset: 200, // how early you want to load image (default = 100)
       errorClass: 'error', // in case of loading image failure what class should be added (default = null)
       successClass: 'success', // in case of loading image success what class should be added (default = null)
       onError: function(image){}, // function fired on loading error
