354e8453116452b3161a9b28e90bbea31e95ede8
jupitech <jupitech.dev@gmail.com>
2016-09-28 16:17:16 -0600

    Orden Consignacion

---
+++ database/migrations/2016_09_28_161320_create_orconsignacion_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrconsignacionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orden_consignacion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_consignacion')->unsigned()->index();
+            $table->foreign('id_consignacion')->references('id')->on('consignacion');
+            $table->integer('id_user')->unsigned()->index();
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->timestamp('fecha_entrega');
+            $table->float('total_compra');
+            $table->integer('estado_orden');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('orden_consignacion');
+    }
+}
