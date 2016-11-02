fdb1514a3dd17b0403a333ca155181ba334b885b
jupitech <jupitech.dev@gmail.com>
2016-11-02 11:09:52 -0600

    Correcciones en reporte de ventas

--- app/Http/Controllers/ReporteVentasController.php
+++ app/Http/Controllers/ReporteVentasController.php
@@ -77,6 +77,8 @@ class ReporteVentasController extends Controller
          return response()->json(['data' =>  $ventas],200);
     }
 
+
+
      public function total()
     {
          $ventas = Ventas::where('estado_ventas',2)
@@ -92,6 +94,7 @@ class ReporteVentasController extends Controller
 
          return response()->json(['data' =>  $ventas],200);
     }
+
 
       public function descuentos()
     {
@@ -110,69 +113,6 @@ class ReporteVentasController extends Controller
          return response()->json(['data' =>  $ventas],200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
