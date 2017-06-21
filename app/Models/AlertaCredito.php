8ad1e8db297d68a60b7c41282b63e1532493b2fc
jupitech <jupitech.dev@gmail.com>
2017-06-21 06:35:10 -0600

    Alerta credito!

--- app/Models/AlertaCredito.php
+++ app/Models/AlertaCredito.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class AlertaCredito extends Model
 {
-    //
+             /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'alerta_credito';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['id_credito','fecha_credito','estado_alerta'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['created_at','updated_at'];
+
+       public function Credito(){
+        return $this->hasOne('App\Models\CreditosVentas','id','id_credito')->with("Ventas");
+    }
 }
